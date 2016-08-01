30adcb9fe668c4896d712fc82bb6e2f74122e624
JohnJeong123 <frenchofkiss@gmail.com>
2016-08-01 15:16:40 +0900

    added GetChargeInfo API

---
+++ getChargeInfo.py
@@ -0,0 +1,29 @@
+
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+imp.reload(sys)
+try: sys.setdefaultencoding('UTF8')
+except Exception as E: pass
+
+import testValue
+
+from popbill import StatementService, PopbillException
+
+statementService =  StatementService(testValue.LinkID,testValue.SecretKey)
+statementService.IsTest = testValue.IsTest
+
+try:
+    print("=" * 15 + " 과금정보 확인 " + "=" * 15)
+
+    ItemCode = "121" # 명세서 코드, [121-거래명세서], [122-청구서], [123-견적서] [124-발주서], [125-입금표], [126-영수증]
+
+    response = statementService.getChargeInfo(testValue.testCorpNum, ItemCode, testValue.testUserID)
+
+    print(" unitCost (단가) : %s" % response.unitCost)
+    print(" chargeMethod (과금유형) : %s" % response.chargeMethod)
+    print(" rateSystem (과금제도) : %s" % response.rateSystem)
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code , PE.message))
