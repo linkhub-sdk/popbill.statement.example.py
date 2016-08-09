b87bf558c45af0fbcfef93f77a234fb7b175bdb0
JohnJeong123 <frenchofkiss@gmail.com>
2016-08-09 11:06:14 +0900

    added GetPopbillURL comment

--- getPopbillURL.py
+++ getPopbillURL.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# code for console Encoding difference. Dont' mind on it 
+# code for console Encoding difference. Dont' mind on it
 import sys
 import imp
 imp.reload(sys)
@@ -16,10 +16,12 @@ statementService.IsTest = testValue.IsTest
 try:
     print("팝빌 URL 확인")
 
-    TOGO = "CHRG" # LOGIN-팝빌 로그인, CHRG-포인트충전 
+    # LOGIN-팝빌 로그인, CHRG-포인트충전, SEAL-인감 및 첨부문서 등록
+    TOGO = "CHRG"
+
     url = statementService.getPopbillURL(testValue.testCorpNum,testValue.testUserID, TOGO)
 
     print("URL: %s" % url)
-    
+
 except PopbillException as PE:
     print("Exception Occur : [%d] %s" % (PE.code , PE.message))
