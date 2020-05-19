c67fb9d59faf8c3a5124b621ca71605964bea116
JohnJeong123 <frenchofkiss@gmail.com>
2020-05-19 14:09:42 +0900

    added getViewURL API

---
+++ getViewURL.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# code for console Encoding difference. Dont' mind on it
+import sys
+import imp
+
+imp.reload(sys)
+try:
+    sys.setdefaultencoding('UTF8')
+except Exception as E:
+    pass
+
+import testValue
+
+from popbill import StatementService, PopbillException
+
+statementService = StatementService(testValue.LinkID, testValue.SecretKey)
+statementService.IsTest = testValue.IsTest
+statementService.IPRestrictOnOff = testValue.IPRestrictOnOff
+
+'''
+1건의 전자명세서 보기 팝업 URL을 반환합니다. [메뉴/버튼 제외]
+- 보안정책으로 인해 반환된 URL의 유효시간은 30초입니다.
+- https://docs.popbill.com/statement/python/api#GetViewURL
+'''
+
+try:
+    print("=" * 15 + " 전자명세서 보기 팝업 URL " + "=" * 15)
+
+    # 팝빌회원 사업자번호
+    CorpNum = testValue.testCorpNum
+
+    # 명세서 코드, 121-명세서, 122-청구서, 123-견적서, 124-발주서, 125-입금표, 126-영수증
+    ItemCode = 121
+
+    # 전자명세서 문서번호
+    MgtKey = "20190117-001"
+
+    url = statementService.getViewURL(CorpNum, ItemCode, MgtKey)
+    print("URL: %s" % url)
+
+except PopbillException as PE:
+    print("Exception Occur : [%d] %s" % (PE.code, PE.message))
