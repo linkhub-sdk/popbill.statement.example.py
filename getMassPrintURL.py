2087ea778451a8da8f194a5eec244350ff4077bd
ProWON <pro_won45@naver.com>
2021-10-05 10:03:34 +0900

    fixed comment URL Function

--- getMassPrintURL.py
+++ getMassPrintURL.py
@@ -21,7 +21,7 @@ statementService.UseLocalTimeYN = testValue.UseLocalTimeYN
 
 '''
 다수건의 전자명세서 인쇄팝업 URL을 반환합니다.
-- 보안정책으로 인해 반환된 URL의 유효시간은 30초입니다.
+- 반환되는 URL은 보안 정책상 30초 동안 유효하며, 시간을 초과한 후에는 해당 URL을 통한 페이지 접근이 불가합니다.
 - https://docs.popbill.com/statement/python/api#GetMassPrintURL
 '''
 
