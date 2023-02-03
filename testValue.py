7e5e014b47113618ea0fc4db94128c0d80d3d8b5
박지헌 <jhpark@linkhubcorp.com>
2023-02-03 13:23:22 +0900

    주석 링크 변경

--- testValue.py
+++ testValue.py
@@ -3,7 +3,7 @@
 '''
 팝빌 전자명세서 API Python SDK Example
 
-- Python SDK 연동환경 설정방법 안내 : https://docs.popbill.com/statement/tutorial/python
+- Python SDK 연동환경 설정방법 안내 : https://developers.popbill.com/guide/statement/python/getting-started/tutorial
 - 업데이트 일자 : 2022-08-09
 - 연동 기술지원 연락처 : 1600-9854
 - 연동 기술지원 이메일 : code@linkhubcorp.com
